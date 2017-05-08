42b604a2fecf649bff32ff3ed6f5323cf4241ac2
Gavin Lee <sz110010@gmail.com>
2017-05-09 04:21:50 +0800

    Unpack the msg

--- Algo/hw4/code.cpp
+++ Algo/hw4/code.cpp
@@ -15,22 +15,38 @@ int main()
     unsigned char c;
     //cout << length << endl;
     //for(int i = 0; i < length; ++i) cout << buffer[i];
-    while(c = *offset++) {
-        cout << (void *)offset << " " << (bitset<8>)c << '|';
-        if(c < 128) {
-            cout << (int)c << " " << (char)c;
-        } else if(c >= 0x90 && c <= 0x9f) { // array
-            cout << (int)(c & 15);
-            cout << " array";
-        } else if(c >= 0xa0 && c <= 0xbf) { // string
-            cout << (bitset<8>)c;
-            //cout << (int)(c & 31);
-            cout << " string";
-        } else {
-            cout << (char)c;
+    int T = (int)*offset++;
+    cout << T << endl;
+    while(T--) {
+        int n = (int)*offset++;
+        int m = (int)*offset++;
+        cout << n << ' ' << m << endl;
+        offset++;
+        for(int i = 0; i < n; ++i) {
+            int sz = (*offset++) & 31; // size of string
+            string str;
+            for(int j = 0; j < sz; ++j)
+                str += (char)*offset++;
+            cout << str << endl;
         }
-        cout << endl;
+        cout << "----------" << endl;
     }
+    //while(c = *offset++) {
+        //cout << (void *)offset << " " << (bitset<8>)c << '|';
+        //if(c < 128) {
+            //cout << (int)c << " " << (char)c;
+        //} else if(c >= 0x90 && c <= 0x9f) { // array
+            //cout << (int)(c & 15);
+            //cout << " array";
+        //} else if(c >= 0xa0 && c <= 0xbf) { // string
+            //cout << (bitset<8>)c;
+            ////cout << (int)(c & 31);
+            //cout << " string";
+        //} else {
+            //cout << (char)c;
+        //}
+        //cout << endl;
+    //}
     is.close();
     delete[] buffer;
     return 0;
