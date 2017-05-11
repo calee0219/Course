7e8bf81c0a7f54dbb0bed4b8033c2184357c7be3
Gavin Lee <sz110010@gmail.com>
2017-05-11 20:09:44 +0800

    Try false

--- Algo/hw4/code.cpp
+++ Algo/hw4/code.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 bool f_used = false;
-int dp[1000010][2];
+int dp[10010][10010];
+vector<string> TYPE;
+vector<int> COST, ATTA;
 int n, m;
 int F = 5;
 
-void DP(int num)
+int DP(int index, int cost)
 {
+    if(index <= 0 || cost <= 0) return 0;
+    dp[index][cost] = max(DP(index-1, cost), DP(index-1, cost-COST[index])+ATTA[index]);
+    cout << dp[index][cost] << endl;
+    return dp[index][cost];
 }
 
 int main()
@@ -21,10 +29,10 @@ int main()
     is.read (buffer,length);
     char* offset = buffer;
     int T = (int)*offset++;
-    cout << T << endl;
     while(T--) {
-        //f_used = false;
-        int check = (int)*offset++;
+        f_used = false;
+        TYPE.clear(); COST.clear(); ATTA.clear();
+        char check = (int)*offset++;
         if(check == 0xcd) {
             n = (int)*offset++;
             n <<= 8;
@@ -44,24 +52,30 @@ int main()
         } else {
             m = check;
         }
-        cout << n << ' ' << m << endl;
         if((int)*offset++ == 0xdc) {
             offset += 2;
         } else if((int)*offset == 0xdd) {
             offset += 4;
         }
         for(int i = 0; i < n; ++i) {
-            //dp[i][0] = 0; dp[i][1] = 0;
+            dp[i][0] = 0; dp[i][1] = 0;
             int sz = (*offset++) & 31; // size of string
-            //if(sz == 11) {
-            //} else {
-            //}
             string str;
+            int cost, attack, def;
             for(int j = 0; j < sz; ++j)
                 str += (char)*offset++;
-            cout << str << endl;
+            stringstream ss;
+            ss << str;
+            ss >> str;
+            ss >> cost;
+            ss >> attack;
+            ss >> def;
+            TYPE.push_back(str);
+            COST.push_back(cost);
+            ATTA.push_back(attack);
         }
-        cout << "----------" << endl;
+        DP(n, m);
+        cout << dp[n][m] << endl;
     }
     is.close();
     delete[] buffer;
