76f45166a4deca0fc5a1651f795347aea5066014
Gavin Lee <sz110010@gmail.com>
2017-05-15 03:24:11 +0800

    What: Change count to P
    Why:
    
    How:

--- ML/lab3/A.py
+++ ML/lab3/A.py
@@ -8,12 +8,12 @@ from sklearn.naive_bayes import GaussianNB
 
 def Classify(data):
     for i in range(data.shape[0]):
-        data.iloc[i,0] = int(float(data.iloc[i,0]) / 100)
-        data.iloc[i,1] = int(float(data.iloc[i,1]) / 2)
-        data.iloc[i,2] = int(float(data.iloc[i,2]) / 200)
-        data.iloc[i,3] = int(float(data.iloc[i,3]) / 10)
-        data.iloc[i,4] = int(float(data.iloc[i,4]) * 100)
-        data.iloc[i,5] = int(float(data.iloc[i,5]) / 100)
+        data.loc[i,'SS-IN'] = int(float(data.loc[i,'SS-IN']) / 100)
+        data.loc[i,'SED-IN'] = int(float(data.loc[i,'SED-IN']) / 2)
+        data.loc[i,'COND-IN'] = int(float(data.loc[i,'COND-IN']) / 200)
+        data.loc[i,'SS-OUT'] = int(float(data.loc[i,'SS-OUT']) / 10)
+        data.loc[i,'SED-OUT'] = int(float(data.loc[i,'SED-OUT']) * 100)
+        data.loc[i,'COND-OUT'] = int(float(data.loc[i,'COND-OUT']) / 100)
 
 
 if __name__ == "__main__":
@@ -23,8 +23,12 @@ if __name__ == "__main__":
     Classify(data)
     print("\ntrain after classify")
     print(data)
-    # target count
-    t_count = dict(data.iloc[:,6].value_counts())
+    # target count & P
+    target_count = dict(data.iloc[:,6].value_counts())
+    target_P = {}
+    for i in target_count:
+        target_P[i] = target_count[i]/13
+    print(target_P)
     # frequency table
     f_table = []
     for i in range(6):
@@ -34,10 +38,11 @@ if __name__ == "__main__":
                 table_list[data.iloc[j,6]] = {}
             if not (data.iloc[j,i] in table_list[data.iloc[j,6]]):
                 table_list[data.iloc[j,6]][data.iloc[j,i]] = 0
-            table_list[data.iloc[j,6]][data.iloc[j,i]] += 1/t_count[data.iloc[j,6]];
+            table_list[data.iloc[j,6]][data.iloc[j,i]] += 1/target_count[data.iloc[j,6]];
         f_table.append(table_list)
     ord = 0
     for i in f_table:
+        print()
         for j in i:
             for k in i[j]:
                 print("P(", end='')
@@ -47,16 +52,18 @@ if __name__ == "__main__":
                 # print(i[j][k], end='\t')
                 print("%.2f" % i[j][k], end='\t')
             print()
-        print()
         ord += 1
 
     # Read test in
-    test = pd.DataFrame({ 'SS-IN'    : pd.Series([222]),
-                          'SED-IN'   : pd.Series([4.5]),
-                          'COND-IN'  : pd.Series([1518]),
-                          'SS-OUT'   : pd.Series([74]),
-                          'SED-OUT'  : pd.Series([0.25]),
-                          'COND-OUT' : pd.Series([1642]) })
+    test = pd.DataFrame({
+        'SS-IN'    : pd.Series([222]),
+        'SED-IN'   : pd.Series([4.5]),
+        'COND-IN'  : pd.Series([1518]),
+        'SS-OUT'   : pd.Series([74]),
+        'SED-OUT'  : pd.Series([0.25]),
+        'COND-OUT' : pd.Series([1642]),
+        })
+    test.reindex_axis(['SS-IN','SED-IN','COND-IN','SS-OUT','SED-OUT','COND-OUT'], axis=1)
     print("\ntest origin")
     print(test)
     Classify(test)
@@ -64,13 +71,13 @@ if __name__ == "__main__":
     print(test)
 
     # propobility matrix
-    ac = t_count
-    for i in t_count:
+    ac = target_P
+    for i in target_P:
         for j in range(test.shape[1]):
             if test.iloc[0,j] in f_table[j][i]:
                 ac[i] *= f_table[j][i][test.iloc[0,j]]
     for i in ac:
-        print(i, end=" ")
+        print(i, end=": ")
         print(ac[i])
 
     # # used Gausian predict
