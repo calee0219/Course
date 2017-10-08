3facc15d22b0a2f71be192c7b5a2f2bd66cfae56
Gavin Lee <sz110010@gmail.com>
2017-10-08 22:35:40 +0800

    OS h2 create matrix

--- OS/hw1/hw1-2.c
+++ OS/hw1/hw1-2.c
@@ -2,9 +2,48 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/time.h>
+
+#include <vector>
+
+struct Matrax
+{
+	vector<vector<int> > number;
+	Matrax(int dim) {
+		int i, j;
+		for(i = 0; i < dim; ++i) {
+			vector<int> tmp;
+			for(j = 0; j < dim; ++j) {
+				tmp.push_back(j + dim * i);
+			}
+			number.push_back(tmp);
+		}
+	}
+};
+
+void compute(int dimension, int process)
+{
+	Matrax a(dimension), b(dimension);
+	struct timeval start, end;
+	gettimeofday(&start, 0);
+	// compute a * b
+
+	gettimeofday(&end, 0);
+	int sec = end.tv_sec - start.tv_sec;
+	int usec = end.tv_usec - start.tv_usec;
+	printf("%d-process, checksum = %d\n", dimension, checksum);
+	printf("elapsed %f ms\n", sec * 1000 + (usjec / 1000.0));
+	return;
+}
 
 int main(void)
 {
-    pid_t = getpid();
+    printf("Dimension: ");
+    int dim;
+    scanf("%d", &dim);
+    compute(dim, 1);
+    compute(dim, 4);
     return 0;
 }
